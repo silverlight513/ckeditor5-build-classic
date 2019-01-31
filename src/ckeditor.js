09f5e43001ac35954daf0d50f1ddba9bd8bff6b5
Jack Rimell <jrimell@adgistics.com>
2019-01-31 13:52:44 +0000

    added functionality to match old adgistics editor

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,94 +1,99 @@
-/**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md.
- */
-
-// The editor creator to use.
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
+import Undo from '@ckeditor/ckeditor5-undo/src/undo';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
-	Autoformat,
+	Undo,
 	Bold,
 	Italic,
-	BlockQuote,
-	CKFinder,
-	EasyImage,
 	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
 	Link,
 	List,
-	MediaEmbed,
 	Paragraph,
-	PasteFromOffice,
-	Table,
-	TableToolbar
+	Alignment,
+	Code,
+	Highlight
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
+			'undo',
+			'redo',
+			'|',
 			'heading',
 			'|',
 			'bold',
 			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'imageUpload',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo'
-		]
-	},
-	image: {
-		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
+			'highlight',
 			'|',
-			'imageTextAlternative'
+			'alignment:left',
+			'alignment:center',
+			'alignment:right',
+			'alignment:justify',
+			'|',
+			'numberedList',
+			'bulletedList',
+			'|',
+			'link',
+			'|',
+			'code'
 		]
 	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
+	highlight: {
+		options: [
+			{
+				model: 'redPen',
+				class: 'pen-red',
+				title: 'Red pen',
+				color: 'red',
+				type: 'pen'
+			},
+			{
+				model: 'bluePen',
+				class: 'pen-blue',
+				title: 'Blue pen',
+				color: 'blue',
+				type: 'pen'
+			},
+			{
+				model: 'pinkPen',
+				class: 'pen-pink',
+				title: 'Pink pen',
+				color: 'pink',
+				type: 'pen'
+			},
+			{
+				model: 'orangePen',
+				class: 'pen-orange',
+				title: 'Orange pen',
+				color: 'orange',
+				type: 'pen'
+			},
+			{
+				model: 'greenPen',
+				class: 'pen-green',
+				title: 'Green pen',
+				color: 'green',
+				type: 'pen'
+			}
 		]
 	},
-	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
